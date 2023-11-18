580040cc207c67d0a7f097e86aa44a6c22e95693
SamB133 <64711289+SamB133@users.noreply.github.com>
2023-11-18 18:41:48 -0500

    working query search; removed alerts

--- src/app.js
+++ src/app.js
@@ -6,8 +6,61 @@ const port = 3000
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    connection.query('SELECT * FROM player', function (error, results, fields) {
+const generateWhereClause = (query) =>
+    Object.keys(query)
+        .filter(key => query[key] !== '*')
+        .map(key =>
+            `${key} LIKE '%${query[key]}%'`
+        ).join(' AND ');
+
+app.get('/api/player', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM player WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results);
+    });
+})
+
+app.get('/api/team', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM team WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results);
+    });
+})
+
+app.get('/api/manager', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM manager WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results);
+    });
+})
+
+app.get('/api/match', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM match WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results);
+    });
+})
+
+app.get('/api/participatedIn', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM participatedIn WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        res.send(results);
+    });
+})
+
+app.get('/api/playedIn', (req, res) => {
+    console.log(req.query);
+    connection.query('SELECT * FROM playedIn WHERE TRUE AND ' + generateWhereClause(req.query), function (error, results, fields) {
         if (error) throw error;
         console.log(results);
         res.send(results);
