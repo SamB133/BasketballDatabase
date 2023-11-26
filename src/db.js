f458680c0684bc41111a43d913bdc4e4be5df62c
SamB133 <64711289+SamB133@users.noreply.github.com>
2023-11-25 18:42:11 -0600

    update database config

--- src/db.js
+++ src/db.js
@@ -4,7 +4,10 @@ let config = {
   host: process.env.DB_HOST ?? '127.0.0.1',
   user: process.env.DB_USER ?? 'root',
   password: process.env.DB_PASSWORD ?? 'password',
-  database: process.env.DB_NAME ?? 'project4347'
+  database: process.env.DB_NAME ?? 'project4347',
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
 export let connection = createConnection(config);
 
