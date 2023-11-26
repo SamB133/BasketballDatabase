41d759834ff97e81abc77d6f852d7e82ac74b7c9
SamB133 <64711289+SamB133@users.noreply.github.com>
2023-11-25 18:22:30 -0600

    edit database config

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import { createConnection } from 'mysql2';
 
 let config = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'password',
-  database: 'project4347'
+  host: process.env.DB_HOST ?? '127.0.0.1',
+  user: process.env.DB_USER ?? 'root',
+  password: process.env.DB_PASSWORD ?? 'password',
+  database: process.env.DB_NAME ?? 'project4347'
 }
 export let connection = createConnection(config);
 
