8448b446c0b63af787c03162687ce6df53535d1d
SamB133 <64711289+SamB133@users.noreply.github.com>
2023-11-25 18:05:29 -0600

    change cloud port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { connection } from './db.js';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ?? 3000
 
 app.use(express.static('public'))
 
